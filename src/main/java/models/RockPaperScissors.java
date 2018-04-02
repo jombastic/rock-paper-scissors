47a827e599ae94eeb6f3c91604ac7d41bf49e4f5
Slavcho Mitrov <slavcho_mitrov@yahoo.com>
2018-04-02 13:51:51 +0200

    Add logic for scissors vs rock

--- src/main/java/models/RockPaperScissors.java
+++ src/main/java/models/RockPaperScissors.java
@@ -1,14 +1,22 @@
 package models;
 
 public class RockPaperScissors {
-  public int evaluateCombos(String player1, String player2) {
+  public String evaluateCombos(String player1, String player2) {
     int player1Score = 0;
+    int player2Score = 0;
+    String winner = null;
+
     boolean player1Favor1 = player1.equals("rock") && player2.equals("scissors");
     boolean player1Favor2 = player1.equals("paper") && player2.equals("rock");
     boolean player1Favor3 = player1.equals("scissors") && player2.equals("paper");
+    boolean player2Favor1 = player1.equals("scissors") && player2.equals("rock");
+    boolean player2Favor2 = player1.equals("rock") && player2.equals("paper");
+    boolean player2Favor3 = player1.equals("paper") && player2.equals("scissors");
     if (player1Favor1 || player1Favor2 || player1Favor3) {
-      player1Score = 1;
+      winner = "Player 1";
+    } else if (player2Favor1) {
+      winner = "Player 2";
     }
-    return player1Score;
+    return winner;
   }
 }
--- src/test/java/models/RockPaperScissorsTest.java
+++ src/test/java/models/RockPaperScissorsTest.java
@@ -6,23 +6,30 @@ import static org.junit.Assert.*;
 
 public class RockPaperScissorsTest {
   @Test
-  public void evaluateCombos_rockVsScissors_int() {
+  public void evaluateCombos_rockVsScissors_String() {
     RockPaperScissors testRockPaperScissors = new RockPaperScissors();
-    int expected = 1;
+    String expected = "Player 1";
     assertEquals(expected, testRockPaperScissors.evaluateCombos("rock", "scissors"));
   }
 
   @Test
-  public void evaluateCombos_paperVsRock_int() {
+  public void evaluateCombos_paperVsRock_String() {
     RockPaperScissors testRockPaperScissors = new RockPaperScissors();
-    int expected = 1;
+    String expected = "Player 1";
     assertEquals(expected, testRockPaperScissors.evaluateCombos("paper", "rock"));
   }
 
   @Test
-  public void evaluateCombos_scissorsVsPaper_int() {
+  public void evaluateCombos_scissorsVsPaper_String() {
     RockPaperScissors testRockPaperScissors = new RockPaperScissors();
-    int expected = 1;
+    String expected = "Player 1";
     assertEquals(expected, testRockPaperScissors.evaluateCombos("scissors", "paper"));
+  }
+
+  @Test
+  public void evaluateCombos_scissorsVsRock_String() {
+    RockPaperScissors testRockPaperScissors = new RockPaperScissors();
+    String expected = "Player 2";
+    assertEquals(expected, testRockPaperScissors.evaluateCombos("scissors", "rock"));
   }
 }
