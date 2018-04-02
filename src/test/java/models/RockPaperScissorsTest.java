345e86c348a42001cae520271a8cb01c8235b957
Slavcho Mitrov <slavcho_mitrov@yahoo.com>
2018-04-02 13:20:03 +0200

    Add logic for rock vs scissors

---
+++ src/test/java/models/RockPaperScissorsTest.java
@@ -0,0 +1,14 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RockPaperScissorsTest {
+  @Test
+  public void evaluateCombos_rockVsScissors_int() {
+    RockPaperScissors testRockPaperScissors = new RockPaperScissors();
+    int expected = 1;
+    assertEquals(expected, testRockPaperScissors.evaluateCombos("rock", "scissors"));
+  }
+}
